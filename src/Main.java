0957a1ce34e5b069ae0043ce35efe3d5ddb4aea1
vlad <vladislav.nesterov1990@mail.ru>
2023-02-20 14:06:04 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Сделать Pull-request из одной ветки в другую :выполнено" );
+        System.out.println("Создайте коммит и сделайте push, после сделайте reset комита (soft)");
+        System.out.println("Используйсте stash и stash apply\n" +
+                "5) Используйте amend commit");
     }
 }
