86db6fc463bf55d75a269c26c00e3681dd8e28b7
vlad <vladislav.nesterov1990@mail.ru>
2023-02-20 13:54:55 +0300

    Выполнено

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Сделать Pull-request из одной ветки в другую :выполнено" );
+        System.out.println("Создайте коммит и сделайте push, после сделайте reset комита (soft)");
     }
 }
