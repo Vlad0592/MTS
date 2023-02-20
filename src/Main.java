a10e6fbdd9eea3fcdefe881964702248b2eadf8c
vlad <vladislav.nesterov1990@mail.ru>
2023-02-20 13:51:23 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Сделать Pull-request из одной ветки в другую");
+        System.out.println("Сделать Pull-request из одной ветки в другую :выполнено" );
     }
 }
